a40e1ef36cda5107c45bea808864f09b7cdb2322
Adam Krawczyk <akrawczyk@i2asolutions.com>
2019-07-06 16:53:51 +0200

    fixed widget for django 2.2.2

--- codemirror/widgets.py
+++ codemirror/widgets.py
@@ -3,8 +3,8 @@ from django import forms
 from django.conf import settings
 from django.core.serializers.json import json
 
-css = getattr(settings, 'CODEMIRROR_CSS', ['css/codemirror.min.css'])
-js = getattr(settings, 'CODEMIRROR_JS', ['js/codemirror.min.js'])
+css = getattr(settings, 'CODEMIRROR_CSS', ['codemirror/css/codemirror.min.css'])
+js = getattr(settings, 'CODEMIRROR_JS', ['codemirror/js/codemirror.min.js'])
 default_config = getattr(settings, 'CODEMIRROR_CONFIG', {})
 
 
@@ -16,17 +16,17 @@ class CodeMirror(forms.Textarea):
         }
 
     def __init__(self, **kwargs):
-        self.config = dict(default_config)
+        self.config = default_config
         self.config.update(kwargs)
-        super(CodeMirror, self).__init__()
+        super().__init__()
 
-    def render(self, name, value, attrs=None):
-        field = super(CodeMirror, self).render(name, value, attrs)
+    def render(self, name, value, attrs=None, renderer=None):
+        field = super().render(name, value, attrs, renderer)
 
         return """%s
-<script type="text/javascript">
-    CodeMirror.fromTextArea(
-        document.getElementById('%s'), %s
-    );
-</script>
-""" % (field, attrs['id'], json.dumps(self.config))
+    <script type="text/javascript">
+        CodeMirror.fromTextArea(
+            document.getElementById('%s'), %s
+        );
+    </script>
+    """ % (field, attrs['id'], json.dumps(self.config))
